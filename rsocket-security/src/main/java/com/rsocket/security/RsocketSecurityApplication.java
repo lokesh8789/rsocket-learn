fb330cc4d1ed36ebe5949775b9845fe3092851f1
lokesh8789 <lokeshsingh6200@gmail.com>
2025-11-15 01:45:14 +0530

    rsocket-security

---
+++ rsocket-security/src/main/java/com/rsocket/security/RsocketSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.rsocket.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketSecurityApplication.class, args);
+	}
+
+}
